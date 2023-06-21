8bccc482026eeecb589721abab806d02ce25d52e
Paras Dev Tyagi <97456954+noob-coder282@users.noreply.github.com>
2023-06-22 00:22:17 +0530

    Commit message

--- libs/supabaseAdmin.ts
+++ libs/supabaseAdmin.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe';
 import { createClient } from '@supabase/supabase-js';
 
-import { Database } from '@/types_db';
+import { Database } from '@/type_db';
 import { Price, Product } from '@/types';
 
 import { stripe } from './stripe';
